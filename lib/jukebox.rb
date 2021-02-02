aed9b1638db4327b85b5257da75d718b32a9b9a3
Blake Runyon <blakerunyon09@gmail.com>
2021-02-02 16:27:22 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,17 +1,3 @@
-=begin
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
-=end
-
 def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -23,7 +9,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
-  if songs.any? { |e| e === input }
+  if songs.any? { |e| e == input }
     puts input
   elsif input.to_i > 0 && input.to_i < songs.length
     input = input.to_i - 1
@@ -34,14 +20,8 @@ def play(songs)
   end
 end
 
-#play(songs)
-
 def list(songs)
-  count = 1
-  songs.map { |e|
-    puts "#{count}. #{e}"
-    count += 1
-  }
+  songs.map.with_index { |e, i| puts "#{i+1}. #{e}" }
 end
 
 def exit_jukebox
@@ -51,13 +31,14 @@ end
 def run(songs)
   puts "Please enter a command:"
   input = gets.chomp
-  if input === "help"
+  case input
+  when "help"
     help
-  elsif input === "play"
+  when "play"
     play(songs)
-  elsif input === "exit"
+  when "exit"
     exit_jukebox
-  elsif input === "list"
+  when "list"
     list(songs)
   end
 end
